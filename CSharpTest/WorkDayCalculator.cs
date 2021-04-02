fb9ec390f4ca75d71d43beaed265e5a3924cadfe
D3N1s1s <kozyrevd2001@ukr.net>
2021-04-02 13:15:23 +0300

    Добавьте файлы проекта.

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime endDate = default;
+            if (weekEnds != null)
+            {
+                List<DateTime> list = new List<DateTime>();
+                List<DateTime> weeks = new List<DateTime>();
+                list.Add(startDate);
+
+                //Adding days without weekends
+                for (int i = 1; i < dayCount; i++)
+                {
+                    list.Add(startDate.AddDays(i));
+                }
+                //Adding weekends
+                for (int i = 0; i < weekEnds.Length; i++)
+                {
+                    for (int j = 0; i < weekEnds[i].EndDate.Day - weekEnds[i].StartDate.Day; j++)
+                    {
+                        weeks.Add(weekEnds[i].StartDate.AddDays(j));
+                        var temp = weekEnds[i].StartDate.AddDays(j);
+                        if (temp == weekEnds[i].EndDate)
+                        {
+                            break;
+                        }
+                    }
+                }
+                ///Insert weekends into day's list and get full collection with dates with weekends
+                var count = 0;
+                for (int i = 0; i < list.Count+weeks.Count; i++)
+                {
+                    if (list[i].Date == weeks[0].Date)
+                    {
+                        foreach (var item in weeks)
+                        {
+                            list.Insert(count,item);
+                        }
+                        break;
+                    }
+                    count++;
+                }
+                //Calculating end date
+                endDate = list[0].AddDays(list.Count() - 1);
+                return endDate;
+            }
+            //If array of weekends is null - return calculated end date
+            else
+            {
+                endDate = startDate.AddDays(dayCount - 1);
+                return endDate;
+            }
+        }
+    }
+}
---
+++ Exec/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpTest;
+
+namespace Exec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 10;
+            DateTime startDate = new DateTime(2021, 12, 1);
+            WorkDayCalculator wdc = new WorkDayCalculator();
+            var res = wdc.Calculate(startDate, count, null);
+            Console.WriteLine(res);
+            int count1 = 10;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2021, 4, 23), new DateTime(2021, 4, 25))
+            };
+            DateTime startDate1 = new DateTime(2021, 4, 21);
+            WorkDayCalculator wdc1 = new WorkDayCalculator();
+            var res1 = wdc.Calculate(startDate1, count1, weekends);
+            //Console.WriteLine(res1);
+            //List<int> ints = new List<int> { 1, 2, 3, 4, 5, 6, 7};
+            //List<int> list = new List<int> {12, 13, 14};
+            ////ints[0] = 5;
+            //foreach (var item in ints)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+
+            //var count = 0;
+            //for (int i = 0; i < ints.Count; i++)
+            //{
+            //    if (ints[i] == 3)
+            //    {
+            //        var l = Enumerable.Range(list[0], list.Count).ToList();
+            //        l.Sort();
+            //        foreach (var item in l)
+            //        {
+            //            ints.Insert(count,item);
+            //        }
+            //        break;
+            //    }
+            //    count++;
+            //}
+
+            //Console.WriteLine();
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //foreach (var item in ints)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+
+            Console.Read();
+        }
+    }
+}
